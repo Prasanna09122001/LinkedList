0333699dc536530c31a9ca76a88993d95a2de7be
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-25 15:41:49 +0530

    [Prasanna] Add:Implement the Addition of Data to the List

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -8,6 +8,11 @@ namespace LinkedList
         static void Main()
         {
             Console.WriteLine("Welcome to the Linked list Problem");
+            linkedlist list = new linkedlist();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
