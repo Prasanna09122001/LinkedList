3c947b6c384ea5e5d01c252fa2006da5c748172c
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-25 17:29:49 +0530

    [Prasanna] Add: Implementation of Reversible of Listed Elements

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Concurrent;
-
-namespace LinkedList
-{
-    class program
-    {
-        static void Main()
-        {
-            Console.WriteLine("Welcome to the Linked list Problem");
-            linkedlist list = new linkedlist();
-            //list.Add(56);
-            //list.Add(30);
-            //list.Add(70);
-            list.AddInReverseOrder(56);
-            list.AddInReverseOrder(30);
-            list.AddInReverseOrder(70);
-            list.Display();
-        }
-    }
-}
+﻿using System;                                                        
+using System.Collections.Concurrent;                                 
+                                                                     
+namespace LinkedList                                                 
+{                                                                    
+    class program                                                    
+    {                                                                
+        static void Main()                                           
+        {                                                            
+             Console.WriteLine("Welcome to the Linked list Problem");
+            linkedlist list = new linkedlist();                      
+            //list.Add(56);                                          
+            //list.Add(30);                                          
+            //list.Add(70);                                          
+            list.AddInReverseOrder(56);                              
+            list.AddInReverseOrder(30);                              
+            list.AddInReverseOrder(70);                              
+            list.Display();                                          
+        }                                                            
+    }                                                                
+}                                                                    
