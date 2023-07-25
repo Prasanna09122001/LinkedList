c37716adc38e469b0eadae94ab66880386300cad
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-07-25 15:34:24 +0530

    [Prasanna] Add:Structure Folder Created

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace LinkedList
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Linked list Problem");
+        }
+    }
+}
